aabb8caf5b3c47712592204b80fd29b5cc7e4b2f
Carlos Gabriel <oditorrinco222@gmail.com>
2026-01-25 20:27:51 -0300

    update!!

--- hyprdots.sh
+++ hyprdots.sh
@@ -2,25 +2,145 @@
 # This script installs the necessary dependences for arch linux hyprland 
 # in next update lets try make the dotfiles to one script not limiting in dependences, lets unifique all in one script 
 
-echo "Root previlages are required to run this script"
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
-    exit
-fi
-echo "Updating system..."
-pacman -Syu --noconfirm
-# yay install 
-if ! command -v yay &> /dev/null
-then
-    echo "Yay could not be found, installing yay..."
-    pacman -S --noconfirm git base-devel
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
+#call for root permission
+root_permision() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root"
+        exit
+    fi
+
+}
+# install yay
+install_yay() {
+    echo "Installing yay"
+    sudo pacman -S --noconfirm git base-devel
+    git clone https://aur.archlinux.org/yay
+    cd yay 
     makepkg -si --noconfirm
-    cd ..
-    mv yay .config/
-fi 
-echo "Installing depences..."
-sudo pacman -S --noconfirm hyprland waybar alacritty hyprshot swww nerd-fonts rofi thunar 
-echo "yay depencences.."
-yay -S --noconfirm waypaper google-chrome 
+    cd .. 
+    mv yay .config
+    echo "yay installed"
+}
+
+install_multilib() {
+    echo "Enabling multilib repository..."
+    sudo sed -i '/\[multilib\]/,/Include = \/etc\/pacman.d\/mirrorlist/s/^#//' /etc/pacman.conf
+    sudo pacman -Syu --noconfirm
+    echo "Multilib repository enabled."
+}
+# tlp configuration function
+configure_tlp() {
+    echo "Configuring TLP for power management..."
+    sudo pacman -S --noconfirm tlp tlp-rdw
+    sudo systemctl enable tlp
+    sudo systemctl start tlp
+    echo "TLP has been configured and started."
+}
+
+install_depencences_pacman() {
+    echo "Installing dependencies for Hyprland..."
+    sudo pacman -S --noconfirm hyprland waybar alacritty rofi thunar nerd-fonts ttf-jetbrains-mono pavucontrol swww
+}
+
+install_depences_aur() {
+    echo "Installing AUR dependencies for Hyprland..."
+    yay -S --noconfirm yay -S google-chrome waypaper
+}
+
+gaming_dependences() {
+    echo "Installing gaming dependencies..."
+    sudo pacman -S --noconfirm gnutls lib32-gnutls base-devel gtk3 lib32-gtk3 python-google-auth python-protobuf \
+        libpulse lib32-libpulse alsa-lib lib32-alsa-lib alsa-utils alsa-plugins lib32-alsa-plugins \
+        giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap openal lib32-openal \
+        libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama libgcrypt lib32-libgcrypt \
+        libgpg-error lib32-libgpg-error ncurses lib32-ncurses mpg123 lib32-mpg123 \
+        libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libva lib32-libva \
+        gst-plugins-base-libs lib32-gst-plugins-base-libs sdl2 lib32-sdl2 v4l-utils lib32-v4l-utils \
+        vulkan-icd-loader lib32-vulkan-icd-loader ocl-icd lib32-ocl-icd opencl-icd-loader lib32-opencl-icd-loader \
+        libxslt lib32-libxslt cups samba lib32-mesa vulkan-radeon lib32-vulkan-radeon \
+        gamescope mangohud lib32-mangohud gamemode lib32-gamemode
+}
+
+setup_terminus() {
+    # 1. Install terminus-font if not present
+    if ! pacman -Qs terminus-font > /dev/null; then
+        printf "%b\n" "${YELLOW}Instalando terminus-font...${RC}"
+        sudo pacman -S --needed --noconfirm terminus-font
+    else
+        printf "%b\n" "${GREEN}Pacote terminus-font já está presente.${RC}"
+    fi
+
+    # 2. system config
+    printf "%b\n" "${YELLOW}Configurando /etc/vconsole.conf...${RC}"
+    if [ -f /etc/vconsole.conf ]; then
+        # Se a linha FONT já existir, substitui. Se não, adiciona ao final.
+        grep -q "^FONT=" /etc/vconsole.conf && \
+        sudo sed -i 's/^FONT=.*/FONT=ter-v18b/' /etc/vconsole.conf || \
+        echo "FONT=ter-v18b" | sudo tee -a /etc/vconsole.conf
+    else
+        echo "FONT=ter-v18b" | sudo tee /etc/vconsole.conf
+    fi
+
+    # 3. Aplicação Imediata (Somente se estiver em TTY)
+    if [ -z "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+        sudo setfont ter-v18b
+        printf "%b\n" "${GREEN}Fonte aplicada ao console atual.${RC}"
+    fi
+
+    printf "%b\n" "${GREEN}Configuração da fonte concluída!${RC}"
+}
+
+# Make hyprland dotfiles copy 
+copy_hyprland_dotfiles() {
+    echo "Copying Hyprland dotfiles..."
+    mkdir -p $HOME/.config/hypr
+    cp -r .config/hypr/* $HOME/.config/hypr/
+    echo "Hyprland dotfiles copied."
+}
+# alacritty copy 
+copy_alacritty_dotfiles() {
+    echo "Copying Alacritty dotfiles..."
+    mkdir -p $HOME/.config/alacritty
+    cp -r .config/alacritty/* $HOME/.config/alacritty/
+    echo "Alacritty dotfiles copied."
+}
+# waybar copy 
+copy_waybar_dotfiles() {
+    echo "Copying Waybar dotfiles..."
+    mkdir -p $HOME/.config/waybar
+    cp -r .config/waybar/* $HOME/.config/waybar/
+    echo "Waybar dotfiles copied."
+}
+# rofi copy 
+copy_rofi_dotfiles() {
+    echo "Copying Rofi dotfiles..."
+    mkdir -p $HOME/.config/rofi
+    cp -r .config/rofi/* $HOME/.config/rofi/
+    echo "Rofi dotfiles copied."
+}
+# fastfetch copy
+copy_fastfetch_dotfiles() {
+    echo "Copying Fastfetch dotfiles..."
+    mkdir -p $HOME/.config/fastfetch
+    cp -r .config/fastfetch/* $HOME/.config/fastfetch/
+    echo "Fastfetch dotfiles copied."
+}
+# Main function 
+main() {
+    root_permision
+    install_yay
+    install_multilib
+    configure_tlp
+    install_depencences_pacman
+    Install_depences_aur
+    gaming_dependences
+    setup_terminus
+    copy_hyprland_dotfiles
+    copy_alacritty_dotfiles
+    copy_waybar_dotfiles
+    copy_rofi_dotfiles
+    copy_fastfetch_dotfiles
+    echo "Hyprland setup completed!"
+}
+
+
