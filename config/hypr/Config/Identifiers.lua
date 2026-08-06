7169789ff176fb4705b53e4efe47860094d6f869
Carlos Gabriel <csouzadsx@protonmail.com>
2026-08-06 20:37:38 -0300

    rafactor some identifiers

--- config/hypr/Config/Identifiers.lua
+++ config/hypr/Config/Identifiers.lua
@@ -11,7 +11,6 @@ Polkit = "systemctl --user start hyprpolkitagent.service"
 Config = "[float;size 1800 1000] " .. Terminal .. " nvim ~/.config/hypr/hyprland.lua"
 XDPH = "/usr/lib/xdg-desktop-portal-hyprland"
 Listener = "~/Scripts/ColorGen/Listener"
-Discord = "ELECTRON_OZONE_PLATFORM_HINT=wayland discord"
 
 local ids = {
 	mainMod = "SUPER",
@@ -28,9 +27,13 @@ local ids = {
 function ids.set_random_wallpaper(opts)
 	opts = opts or {}
 	local handle = io.popen(
-		'find "' .. ids.wallpaperDir .. '" -type f \\( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.webp" \\)'
+		'find "'
+			.. ids.wallpaperDir
+			.. '" -type f \\( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.webp" \\)'
 	)
-	if not handle then return end
+	if not handle then
+		return
+	end
 
 	local arquivos = {}
 	for linha in handle:lines() do
